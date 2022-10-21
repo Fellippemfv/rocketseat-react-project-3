235d3136cf932dd0b587b28c63f937f908d5d990
Fellippe Matheus <Fellippe.matheus2014@gmail.com>
2022-10-20 21:32:16 -0400

    Create component: Searchform

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHightlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
@@ -9,31 +10,33 @@ export function Transactions() {
             <Summary />
 
            <TransactionsContainer>
-            <TransactionsTable>
-                    <tbody>
-                        <tr>
-                            <td width="50%">Desenvolvimento do site</td>
-                            <td>
-                                <PriceHightlight variant="income">
-                                R$ 12.000,00
-                                </PriceHightlight>
-                            </td>
-                            <td>Venda</td>
-                            <td>20/10/2022</td>
-                        </tr>
+                <SearchForm />
 
-                        <tr>
-                            <td width="50%">Coxinha</td>
-                            <td>
-                                <PriceHightlight variant="outcome">
-                                    - R$ 59,00
-                                </PriceHightlight>
-                            </td>
-                            <td>Alimentação</td>
-                            <td>15/10/2022</td>
-                        </tr>
-                    </tbody>
-            </TransactionsTable>
+                <TransactionsTable>
+                        <tbody>
+                            <tr>
+                                <td width="50%">Desenvolvimento do site</td>
+                                <td>
+                                    <PriceHightlight variant="income">
+                                    R$ 12.000,00
+                                    </PriceHightlight>
+                                </td>
+                                <td>Venda</td>
+                                <td>20/10/2022</td>
+                            </tr>
+
+                            <tr>
+                                <td width="50%">Coxinha</td>
+                                <td>
+                                    <PriceHightlight variant="outcome">
+                                        - R$ 59,00
+                                    </PriceHightlight>
+                                </td>
+                                <td>Alimentação</td>
+                                <td>15/10/2022</td>
+                            </tr>
+                        </tbody>
+                </TransactionsTable>
            </TransactionsContainer>
         </div>
     )
